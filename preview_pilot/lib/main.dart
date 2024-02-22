9d399e1b2a0ec22aa8d19d34ebd1833ccb66ca78
Libbyclaire McGhee <lcmcghe1@memphis.edu>
2024-02-22 11:20:46 -0600

    Heavy WIP for trying out flutter AR capabilities, not functional yet, just putting this up here

--- preview_pilot/lib/main.dart
+++ preview_pilot/lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:arcore_example/views/homeView.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+	WidgetsFlutterBinding.ensureInitialized();
+  	runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  	const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+  	// This widget is the root of your application.
+  	@override
+  	Widget build(BuildContext context) {
+    	return MaterialApp(
+      		title: 'Flutter Demo',
+      		theme: ThemeData(
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
@@ -28,12 +31,12 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+        	colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        	useMaterial3: true,
+      			),
+      	home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    		);
+  	}
 }
 
 class MyHomePage extends StatefulWidget {
---
+++ preview_pilot/lib/views/localAndWebObjectsView.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class LocalAndWebObjectsView extends StatefulWidget {
+  const LocalAndWebObjectsView({Key? key}) : super(key: key);
+
+  @override
+  State<LocalAndWebObjectsView> createState() => _LocalAndWebObjectsViewState();
+}
+
+class _LocalAndWebObjectsViewState extends State<LocalAndWebObjectsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Local / Web Objects"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .8,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(22),
+                child: Container(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        //TODO
+                      },
+                      child: const Text("Add / Remove Local Object")),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        //TODO
+                      },
+                      child: const Text("Add / Remove Web Object")),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
